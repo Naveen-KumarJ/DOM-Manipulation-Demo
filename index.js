dcd5e3e3e86f6cf8760272905dca74302a8b4ba2
Naveen Kumar J <naveen.jayakumar02@gmail.com>
2025-01-13 23:03:08 +0530

    Done DOM Manipulation Demo Task

---
+++ index.js
@@ -0,0 +1,14 @@
+const targetDiv = document.getElementById("box"); //getting div by getElementById
+targetDiv.style.backgroundColor="yellow"; //Changed the background color of target div
+targetDiv.style.margin = "20px"; //Added a margin of 20 pixels to target div
+targetDiv.style.padding = "10px"; //Added a padding of 10 pixels to target div
+targetDiv.style.fontSize = "18px"; //Changed the font size to 18 pixels
+targetDiv.style.fontWeight = "bold"; //Changed the font weight to bold
+targetDiv.style.height = "200px"; //Changed the height of the element to 200 pixels
+targetDiv.style.width = "300px"; //Changed the width of the element to 300 pixels
+
+
+// Above are changes points mentioned to be done !
+
+targetDiv.style.color = "#0000FF";
+targetDiv.style.paddingTop = "1px";
